cfaa94b9f22451a7917b403f830dd4e5f727908f
Alice <lgurvan81@gmail.com>
2022-11-17 10:52:52 +0100

    remove trailing slash in constant url
    because i prefer so

--- src/main/java/org/mesa/pkwrapper/utils/Constants.java
+++ src/main/java/org/mesa/pkwrapper/utils/Constants.java
@@ -4,5 +4,5 @@ import okhttp3.MediaType;
 
 public class Constants {
     public static MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    public static String BASE_URL = "https://api.pluralkit.me/v2/";
+    public static String BASE_URL = "https://api.pluralkit.me/v2";
 }
